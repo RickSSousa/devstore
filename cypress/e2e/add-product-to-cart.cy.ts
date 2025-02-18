b65a15d62eedb2c8e2d5e173e417e8e9147fb440
Ricardo S. Sousa <ricksantos.rs25@gmail.com>
2025-02-18 11:17:28 -0300

    progress(frontend): add search test

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,13 +1,14 @@
 describe("add product to cart", () => {
+  beforeEach(() => {
+    cy.visit("/"); //configuração aplicada na cypress.config de base url para visitar a página inicial
+  });
   it("should be able to navigate to the product page and add it to the cart", () => {
-    cy.visit("http://localhost:3000");
     cy.get('a[href^="/product"]').first().click();
     cy.location("pathname").should("include", "/product"); // verifica se a URL contém "/product"
     cy.contains("Adicionar ao carrinho").click();
     cy.contains("Cart (1)").should("exist"); // verifica se o texto "Cart (1)" existe na página
   });
   it("should not count duplicated products on cart", () => {
-    cy.visit("http://localhost:3000");
     cy.get('a[href^="/product"]').first().click();
     cy.location("pathname").should("include", "/product");
     cy.contains("Adicionar ao carrinho").click();
@@ -15,7 +16,6 @@ describe("add product to cart", () => {
     cy.contains("Cart (1)").should("exist");
   });
   it("should be able to search for a product and add it to the cart", () => {
-    cy.visit("http://localhost:3000");
     cy.get("input[name=q]").type("moletom").parent("form").submit(); //busca imput, insere "moletom" e submete
     cy.get('a[href^="/product"]').first().click();
     cy.location("pathname").should("include", "/product");
